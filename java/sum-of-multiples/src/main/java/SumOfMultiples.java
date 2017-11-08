b6cb010ed83de5fcc3d3d45b5d98ac8ceffddec2
Ma_tzy <marija.stojchevska@student.kuleuven.be>
2017-11-08 09:35:21 +0100

    solution

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,11 +1,32 @@
+import java.util.HashSet;
+
 class SumOfMultiples {
 
+	int number;
+	int[] set;
+
     SumOfMultiples(int number, int[] set) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.number = number;
+		this.set = set;
     }
 
     int getSum() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        HashSet<Integer> multiples = new HashSet<Integer>();
+		int sum = 0;
+
+		for(int i=1; i<this.number; i++) {
+			for(int j=0; j<set.length; j++) {
+				if(i % set[j] == 0) {
+					multiples.add(i);
+					j = set.length;
+				}
+			}
+		}
+
+		for(Integer i : multiples) {
+			sum += i;	
+		}
+		return sum;
     }
 
 }
